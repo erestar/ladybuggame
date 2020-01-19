7761e3583246cd3d82b51947cad0ed8f367f307c
Jim Podroskey <erestar@gmail.com>
2020-01-19 07:54:03 -0500

    Split up rendering of different card types

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,24 @@
 import React from "react";
+import {pluralize} from "../engine";
+
+const MoveCard = ({card}) => {
+    return (
+        <div>
+            {`Move ahead ${pluralize('space', Math.abs(card.magnitude))}!`}
+            {card.goAgain ? ' Go Again' : ''}
+        </div>
+    );
+};
+
+const AphidCard = ({card}) => {
+    return (
+        <div>
+            {`You ${card.magnitude > 0 ? 'gain' : 'loose' } ${pluralize('aphid', Math.abs(card.magnitude))}!`}
+        </div>
+    );
+};
 
 export const Card = ({card}) => {
-    return (
-        <>
-            {card.type} - {card.magnitude}
-        </>
-    );
+    return (card.type === 'move')? <MoveCard card={card} /> : <AphidCard card={card} />
 };
 
